9c2cc18a90587d6979d21fe05d3576d04b427717
ethanye <ethanye@xiaoe-tech.com>
2021-05-22 10:18:57 +0800

    feat: upgradle build scripts

--- app/src/main/java/com/ethan/android/studyjam/MainActivity.kt
+++ app/src/main/java/com/ethan/android/studyjam/MainActivity.kt
@@ -3,36 +3,86 @@ package com.ethan.android.studyjam
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import com.ethan.android.studyjam.ui.theme.AndroidStudyJamTheme
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidStudyJamTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Surface(Modifier.padding(8.dp)) {
+                NewStory()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    @Preview
+    fun DefaultPreview() {
+        NewStory()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidStudyJamTheme {
-        Greeting("Android")
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Composable
+    fun NewStory() {
+        MaterialTheme {
+            val typography = MaterialTheme.typography
+            Card {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.header),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(180.dp)
+                            .fillMaxWidth()
+                            .clip(shape = RoundedCornerShape(4.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Spacer(modifier = Modifier.height(18.dp))
+
+                    Text(
+                        text = "A day wandering through the sandhills " +
+                                "in Shark Fin Cove, and a few of the " +
+                                "sights I saw",
+                        style = typography.h6,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                    Text(
+                        text = "Davenport, California",
+                        style = typography.body2
+                    )
+
+                    Text(
+                        text = "December 2018",
+                        style = typography.body2
+                    )
+                }
+            }
+        }
     }
 }
